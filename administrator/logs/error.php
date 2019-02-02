9dacce4ae98da87431572abbe8a6f48a188f15c4
FernandaBaez <1630386@upv.edu.mx>
2019-02-02 03:20:32 +0000

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-31 00:01:29 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-31T00:01:29+00:00	INFO 187.198.45.64	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-31T03:12:22+00:00	INFO 187.198.45.64	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-01T04:43:00+00:00	INFO 187.198.45.64	joomlafailure	Username and password do not match or you do not have an account yet.
